eb4b61e9596b307d744637388563a2aee7c46109
Jared West <utroda@MacBook-Air.localdomain>
2023-01-15 20:29:10 -0800

    fixing inital commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,21 @@
+--[[
+Neovim init file
+Version: 0.2.0 - 2022/04/19
+Maintainer: jwest
+Website: https://github.com/jwestjc
+--]]
+
+-- Import Lua modules
+require('packer_init');
+require('core/options');
+require('core/autocmds');
+require('core/keymaps');
+require('core/colors');
+require('plugins/cmp').setup();
+require('plugins/telescope').setup();
+require('plugins/treesitter').setup();
+require('plugins/lspconfig');
+require('plugins/handlers').setup();
+require('plugins/null-ls').setup();
+require('plugins/blankline').setup();
+require('plugins/lualine').setup();
