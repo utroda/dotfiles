95c9e0192de0dc7628b0c07c2189314e9e447b33
utroda <1255024+utroda@users.noreply.github.com>
2023-01-30 15:16:09 -0800

    adding more lsp servers

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,4 +3,6 @@ require("mason-lspconfig").setup()
 
  require("lspconfig").volar.setup {}
  require("lspconfig").astro.setup {}
+ require("lspconfig").sumneko_lua.setup {}
+ require("lspconfig").tsserver.setup {}
 
